ec257a59caa30ef5ddd05c39ae4540531581ade1
David Mimay <davidmimay@gmail.com>
2026-08-29 19:46:13 +0000

    feat(footer): update footer content and styles; add MatButtonModule import

--- src/app/shared/footer/footer.ts
+++ src/app/shared/footer/footer.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-footer',
   standalone: true,
-  imports: [MatIconModule],
+  imports: [MatIconModule, MatButtonModule],
   templateUrl: './footer.html',
   styleUrl: './footer.scss'
 })
