c06f218daddd9377617582f4648b8e1bef13ce45
David Mimay <davidmimay@gmail.com>
2026-08-29 19:36:59 +0000

    feat(footer): add footer component with styles and tests

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,14 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Navbar } from './shared/navbar/navbar';
+import { Footer } from './shared/footer/footer';
 
 @Component({
-  imports: [RouterOutlet, Navbar],
+  imports: [
+    RouterOutlet,
+    Navbar,
+    Footer
+  ],
   selector: 'app-root',
   standalone: true,
   styleUrl: './app.scss',
